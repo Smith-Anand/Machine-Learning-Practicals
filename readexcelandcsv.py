f4b6e8955bf2b98f5482bc6b6133b72cddfb63f1
Smith Anand <111965310+Smith-Anand@users.noreply.github.com>
2026-06-11 12:34:56 +0530

    Add files via upload

---
+++ readexcelandcsv.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+winedatac=pd.read_csv(r'D:/Smith_Anand_ML/wine.csv')
+
+print(winedatac)
+print(winedatac.head())
+print("shape\n",winedatac.shape)
+print("columns\n",winedatac.columns)
+print("dtypes\n",winedatac.dtypes)
+print("ndim\n",winedatac.ndim)
+print("size\n",winedatac.size)
+
+
+winedatae=pd.read_csv(r'D:/Smith_Anand_ML/wine.csv')
+print('\n')
+
+
